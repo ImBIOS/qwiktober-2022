7653dfebb6e343a459faae00fdf1d60f261d8d42
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2022-10-11 22:17:55 +0700

    fix: edge error (#117)
    * fix: edge error
    
    * test contributor validation
    
    * fix validation
    
    * final fix edge error

--- src/data/contributors.ts
+++ src/data/contributors.ts
@@ -82,6 +82,12 @@ const contributor = [
     githubUsername: "Drachzak",
     instagramUsername: "dzaksssssss",
     linkedinUsername: "Dzakwan Rachman"
+  },
+  {
+    fullname: "Dzakwan Daris Rachman",
+    githubUsername: "Drachzak",
+    instagramUsername: "dzaksssssss",
+    linkedinUsername: "Dzakwan Rachman"
   }
 ];
 
