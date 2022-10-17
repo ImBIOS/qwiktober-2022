c97c607e0059a9a87e6ad92a79aa9f91f8b42fe7
farrasrifqi <farrasrifqi25@gmail.com>
2022-10-17 16:47:21 +0700

    adddd

--- src/data/contributor.ts
+++ src/data/contributor.ts
@@ -17,6 +17,12 @@ const contributor = [
     instagramUsername: "risnanto73_",
     linkedinUsername: "tio-risnanto-605515164",
   },
+  {
+    fullname: "Farras Rifqi Amru Hanifah",
+    githubUsername: "farrasrifqi",
+    instagramUsername: "farras_rifqi",
+    linkedinUsername: "farras-rifqi-a91192249",
+  },
 ];
 
 export default contributor;
